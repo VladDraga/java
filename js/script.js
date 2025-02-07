b11dedd1ec2e53dec2370bc3e86b765b43dc2793
VladDraga <draga260806@gmail.com>
2025-02-07 16:41:51 +0100

    dz

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const age = 18;
+console.log(age);
+const name = "Vlad";
+console.log(name);
+const isStudent = "true";
+console.log(isStudent);
+const myString = "Щоб холодна вода перетворилася на гарячу, її потрібно підігріти";
+console.log(myString);
+let myNumber = 5;
+myNumber = myNumber + 10;
+console.log(myNumber);
+const myNull = null;
+console.log(typeof (myNull));
+const firstModal = prompt("Будь ласка, введіть ваше ім'я:");
+alert("Привіт, " + firstModal + "!");
+const secondModal = confirm("Підтвердіть дію, будь ласка:");
+alert("Дякую за підтвердження!");
+const thirdModal = confirm("Дія яку ви хочете зробити може буди небезпечча, підтвердіть її, будь ласка:");
+alert("Дякую за підтвердження!");
+
+
+
+
+
+
